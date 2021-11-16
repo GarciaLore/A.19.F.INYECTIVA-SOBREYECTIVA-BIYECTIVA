7c54f0188257c6645302110f628c6cb1904c8882
GarciaLore <91568849+GarciaLore@users.noreply.github.com>
2021-11-16 13:55:47 -0600

    Función#1
    Para la resolución de esta función lo primero que realice fue la solución a mano, después de ello me dirigí a Octave para poder proseguir con lo que asi falta, y al momento de realizar la actividad a mano me percate de que el tipo de grafica que me salía no pertenecía a ninguna de las tres funciones que nos mencionaba la actividad por lo que decidí investigar mas acerca de ello.

---
+++ funcion1.m
@@ -0,0 +1,33 @@
+% Octave Script 
+% Title             :Funciones, Inyectiba, Sobreyectiva y Biyectiva
+% School            :Colegio de Estudios Superiores de Jilotepec
+% Carer             :Ingenieria en Sistemas Computacionales
+% Authors           :Garcia Garcia Lorena
+% Date              :15/11/2021
+% Version           :1.0
+% Usage             :octave/path/loere
+% Notes             :Se requiere de la aplicacion de Octave para poder dar solucion a los problemas referentes a "LOS TIPOS DE FUNCIONES"
+% Funcion numero    :#1
+%Limpiar variables (LV)
+clear
+%Agregar Paquete Symbolic(APS)
+pkg load symbolic
+syms x
+x=(-20:1:20)
+disp ('valor de funcion');
+disp ('(g(x)=x.^2)');
+disp ('sea g: (-20,20)--> R dada por g(x)=x^2');
+disp ('Este ejercio NO puede ser una funcion INYECTIVA ya que se debe partir del conjunto de salida de cada elemento en la cual su llegada tiene una doble correspondencia ');
+disp ('De la misma manera no puede ser una funcion SOBREYECTIVA ya que los elementos de llegada es decir  "y" son negativos y quedan vacios');
+disp ('Por tanto,con todo lo anterior podemos determinar que tampoco puede ser una funcion biyectiva ya que al no cumplir con ninguna de las dos anteiores funciones, pues queda claro que es una funcion racional');
+gx=(x.^2)
+disp ('Graficar la funcion');
+plot (x, gx);
+
+grid on;
+%Titulo (T)
+title('FUNCION DEL PROBLEMA #1');
+%Etiqueta para x (E.x)
+xlabel('(DOMINIO)VALOR DE X ');
+%Etiqueta para y (E.y)
+ylabel('g(x) o Imagen');
